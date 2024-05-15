e4378884be728706a2ccd05c41cc936620ae2c16
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-05-16 00:26:54 +0100

    Refactor filter_datum function in filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,10 +5,8 @@ import re
 
 
 def filter_datum(fields, redaction, message, separator):
-    '''returns the log message obfuscated
-    '''
+    '''returns the log message obfuscated'''
     for field in fields:
         message = re.sub(field + '=.*?' + separator,
                          field + '=' + redaction + separator, message)
-
     return message
