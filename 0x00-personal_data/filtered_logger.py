002ece1da2ed84730735acc00d9410e0829f4abb
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-05-16 01:20:18 +0100

    filter values in incoming log records using filter_datum.

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,6 +2,7 @@
 '''Filter logging module'''
 import re
 from typing import List
+import logging
 
 
 def filter_datum(
@@ -12,3 +13,23 @@ def filter_datum(
         message = re.sub(field + '=.*?' + separator,
                          field + '=' + redaction + separator, message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        '''format the record
+        '''
+        record.msg = filter_datum(self.fields, self.REDACTION,
+                                  record.getMessage(), self.SEPARATOR)
+        return logging.Formatter(self.FORMAT).format(record)
