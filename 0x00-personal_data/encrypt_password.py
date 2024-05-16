4079096c2462f0650575adae319a200b13858fa5
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-05-16 19:57:49 +0100

    Add is_valid function for password validation 🔥🔥

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -8,3 +8,9 @@ def hash_password(password: str) -> bytes:
     '''hash_password function
     '''
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    '''is_valid function
+    '''
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
