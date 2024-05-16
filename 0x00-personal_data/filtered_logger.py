e9d1ad984c92edf794c390595f9c162cd609ce1e
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-05-16 02:45:23 +0100

    trying to fix a checker while everything is working as expected

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -50,16 +50,12 @@ def get_logger() -> logging.Logger:
 
 
 def get_db() -> mysql.connector.connection.MySQLConnection:
-    '''returns a connector to a db
-    '''
+    '''returns a connector to a db'''
     username = os.getenv('PERSONAL_DATA_DB_USERNAME', 'root')
     password = os.getenv('PERSONAL_DATA_DB_PASSWORD', '')
     host = os.getenv('PERSONAL_DATA_DB_HOST', 'localhost')
     db_name = os.getenv('PERSONAL_DATA_DB_NAME')
 
     return mysql.connector.connect(
-        user=username,
-        password=password,
-        host=host,
-        database=db_name
+        user=username, password=password, host=host, database=db_name
     )
