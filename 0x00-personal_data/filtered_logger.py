825360f741e97d157d73f925b8acf3a0f6463e5d
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-05-16 01:22:23 +0100

    adding doc to methods

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -17,19 +17,19 @@ def filter_datum(
 
 class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter class
-        """
+    """
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
     def __init__(self, fields):
+        '''initialize __init__ '''
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        '''format the record
-        '''
+        '''format the record'''
         record.msg = filter_datum(self.fields, self.REDACTION,
                                   record.getMessage(), self.SEPARATOR)
         return logging.Formatter(self.FORMAT).format(record)
