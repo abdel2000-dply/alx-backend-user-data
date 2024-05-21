0d80aaa4de0ac7ac3fbf209fe321fdadc23af138
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-05-21 16:13:31 +0100

    update authentication module with basic functionality

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -12,6 +12,7 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
 
 
 @app.errorhandler(404)
@@ -33,6 +34,31 @@ def forbidden(error) -> str:
     """ Forbidden handler
     """
     return jsonify({"error": "Forbidden"}), 403
+
+
+auth_type = getenv('AUTH_TYPE')
+
+if auth_type == 'auth':
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+
+
+@app.before_request
+def before_request() -> None:
+    """ Before request function
+    """
+    if auth is None:
+        return
+
+    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+    if not auth.require_auth(request.path, excluded_paths):
+        return
+
+    if auth.authorization_header(request=request) is None:
+        abort(401)
+
+    if auth.current_user(request) is None:
+        abort(403)
 
 
 if __name__ == "__main__":
