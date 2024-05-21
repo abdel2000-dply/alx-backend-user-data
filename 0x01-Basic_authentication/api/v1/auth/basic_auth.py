0ee87464fb333211a73b298ed9acb9856806e889
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-05-21 22:51:51 +0100

    Implement current_user method in BasicAuth class
    This commit adds the `current_user` method to the `BasicAuth` class in the `basic_auth.py` file.

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -73,3 +73,12 @@ class BasicAuth(Auth):
             return user
         except Exception:
             return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Current user
+        """
+        auth_header = self.authorization_header(request)
+        base64_header = self.extract_base64_authorization_header(auth_header)
+        decoded_header = self.decode_base64_authorization_header(base64_header)
+        email, pwd = self.extract_user_credentials(decoded_header)
+        return self.user_object_from_credentials(email, pwd)
