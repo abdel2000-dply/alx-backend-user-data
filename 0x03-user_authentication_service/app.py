97eec7388f007019b705fdf53483c8928fb3702c
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-05-28 17:01:35 +0100

    Update HTTP method for reset password endpoint

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -86,7 +86,7 @@ def get_reset_password_token():
         abort(403)
 
 
-@app.route("/reset_password", methods=['put'], strict_slashes=False)
+@app.route("/reset_password", methods=['PUT'], strict_slashes=False)
 def update_password():
     """ PUT /reset_password
         Return: update password
